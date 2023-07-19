0bcc7149f7765366e5483595a09672df76c7b9bb
jvasquezs <68759477+jvasquezs@users.noreply.github.com>
2023-07-19 18:32:17 -0300

    Update elecciones.py

--- elecciones.py
+++ elecciones.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import streamlit as st
 
+try:
+  mesas=pd.read_csv(mesas.csv,index=0)
+except:
+  mesas=pd.DataFrame(index=range(1,1000), col_names=["Votos","Cernadas","Cufre"])
+
 st.title("""Escrutinio Rapido""")
 mesa = st.number_input("""Mesa: """, min_value=1, max_value=900, help="""Número de mesa""")
 votos_totales = st.number_input("""Votantes: """, value=1, min_value=1, max_value=400, help="""Cantidad de votantes""")
@@ -11,3 +16,5 @@ title = st.text_input('Comentarios adicionales: ', '')
 
 if st.button('Guardar'):
   st.write('El porcentaje de votos de JxC es ', ((votos_cernadas+votos_cufre)/votos_totales-1)*100)
+  mesas.loc[mesa] = [votos_totales,votos_cernadas,votos_cufre]
+  
